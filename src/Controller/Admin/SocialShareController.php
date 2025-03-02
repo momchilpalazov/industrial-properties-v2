c1548909cab84952ffa3cdb6fe68e4f1f6a0bbcd
Momchil Palazov <momchil.palazov@gmail.com>
2025-03-02 14:08:36 +0100

    Refactor social share route and add navigation for social posts in admin panel

--- src/Controller/Admin/SocialShareController.php
+++ src/Controller/Admin/SocialShareController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/admin/social')]
+#[Route('/social')]
 class SocialShareController extends AbstractController
 {
     private FacebookService $facebookService;
