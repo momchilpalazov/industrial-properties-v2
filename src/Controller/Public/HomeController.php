7b4510072bb0d8f93ccf925b6455405488212005
Momchil Palazov <momchil.palazov@gmail.com>
2025-06-26 00:08:24 +0200

    feat: Enhance multilingual support for About page with new translations and dynamic content rendering

--- src/Controller/Public/HomeController.php
+++ src/Controller/Public/HomeController.php
@@ -59,7 +59,39 @@ class HomeController extends AbstractController
         $settings = $this->aboutSettingsRepository->getSettings();
         
         return $this->render('home/about.html.twig', [
-            'settings' => $settings
+            'settings' => [
+                'title_bg' => $settings->getTitleBg(),
+                'title_en' => $settings->getTitleEn(),
+                'title_de' => $settings->getTitleDe(),
+                'title_ru' => $settings->getTitleRu(),
+                'subtitle_bg' => $settings->getSubtitleBg(),
+                'subtitle_en' => $settings->getSubtitleEn(),
+                'subtitle_de' => $settings->getSubtitleDe(),
+                'subtitle_ru' => $settings->getSubtitleRu(),
+                'content_bg' => $settings->getContentBg(),
+                'content_en' => $settings->getContentEn(),
+                'content_de' => $settings->getContentDe(),
+                'content_ru' => $settings->getContentRu(),
+                'company_image' => $settings->getCompanyImage(),
+                'values_bg' => $settings->getValuesBg(),
+                'values_en' => $settings->getValuesEn(),
+                'values_de' => $settings->getValuesDe(),
+                'values_ru' => $settings->getValuesRu(),
+                'team' => $settings->getTeam(), // За обратна съвместимост
+                'team_bg' => $settings->getTeamBg(),
+                'team_en' => $settings->getTeamEn(),
+                'team_de' => $settings->getTeamDe(),
+                'team_ru' => $settings->getTeamRu(),
+                'team_common' => $settings->getTeamCommon(),
+                'meta_title_bg' => $settings->getMetaTitleBg(),
+                'meta_title_en' => $settings->getMetaTitleEn(),
+                'meta_title_de' => $settings->getMetaTitleDe(),
+                'meta_title_ru' => $settings->getMetaTitleRu(),
+                'meta_description_bg' => $settings->getMetaDescriptionBg(),
+                'meta_description_en' => $settings->getMetaDescriptionEn(),
+                'meta_description_de' => $settings->getMetaDescriptionDe(),
+                'meta_description_ru' => $settings->getMetaDescriptionRu()
+            ]
         ]);
     }
 
